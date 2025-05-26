84b9c0b31726a4c018ffd6ad9c92294e8a29e58f
Katja Konermann <k.konermann@dnb.de>
2025-05-26 13:12:39 +0200

    Use less options in preprocess script

--- preprocess.py
+++ preprocess.py
@@ -13,35 +13,30 @@ from utils import init_tokenizer
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-parser = argparse.ArgumentParser(description="Train a model on the GND dataset.")
+parser = argparse.ArgumentParser(description="Create a dataset.")
 parser.add_argument("--data_dir", type=str, help="Path to the GND dataset directory.")
 parser.add_argument("--gnd_graph", type=str, help="Path to the GND graph file (pickle).")
 parser.add_argument("--config", type=str, help="Path to the configuration file.")
 parser.add_argument("--out_dir", type=str, help="Path to the out directory.")
-parser.add_argument("--context", default=None, help="Context added to the dataset, choose from text or graph", choices=["text", "graph"])
-parser.add_argument("--hops", default=0, type=int, help="Number of hops to use when adding context.")
-parser.add_argument("--relation", default=None, type=str, help="Relations to use when adding context.")
-parser.add_argument("--top_k", default=1, type=int, help="Number of top k neighbors to use when adding context.")
-parser.add_argument("--mapping", type=str, help="Path to the label mapping file.")
-parser.add_argument("--index", type=str, help="Path to the index file.")
-parser.add_argument("--title_wise", action="store_true", help="Use title wise context.")
 
 arguments = parser.parse_args()
 data_dir = arguments.data_dir
 gnd_graph = arguments.gnd_graph
 config_path = arguments.config
 out_dir = arguments.out_dir
-context = arguments.context
-hops = arguments.hops
-relation = arguments.relation
-top_k = arguments.top_k
-mapping_path = arguments.mapping
-index_path = arguments.index
-title_wise = arguments.title_wise
 
 # Load config 
 with open(config_path, "r") as f:
     config = yaml.safe_load(f)
+
+context_config = config["context"]
+context = context_config["context_type"]
+hops = context_config["hops"]
+relation = context_config["relation"]
+top_k = context_config["top_k"]
+mapping_path = context_config["mapping_path"]
+index_path = context_config["index_path"]
+title_wise = context_config["title_wise"]
 
 gnd_graph = pickle.load(open(gnd_graph, "rb"))
 tokenizer = init_tokenizer(config["model_name"])
@@ -71,9 +66,9 @@ if context is not None:
         k=top_k,
         context_type=context,
         use_title_wise=title_wise,
+        batch_size=1024,
         )
 
-# TODO: Tokenization with context?
 # Tokenize the datasets
 gnd_ds.tokenize_datasets(tokenizer=tokenizer, splits=["train", "validate"])
 gnd_ds.inference_tokenize_datasets(tokenizer=tokenizer, splits=["test"])
