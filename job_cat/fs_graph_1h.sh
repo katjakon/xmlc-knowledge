54718fdad3581d00cd6d0980b13c0c58d5a9b2ea
Katja Konermann <katjakonermann@gmail.com>
2025-11-25 17:51:58 +0100

    Add config and job

--- job_cat/fs_graph_1h.sh
+++ job_cat/fs_graph_1h.sh
@@ -14,10 +14,10 @@
 module purge
 module load release/24.04  GCCcore/11.3.0
 module load Python/3.10.4
-export HF_HOME=/data/cat/ws/kako402f-thesis-cat/.cache/
-source /data/cat/ws/kako402f-thesis-cat/xmlc-knowledge/env/bin/activate
+export HF_HOME=/data/cat/ws/kako402f-thesis/.cache/
+source /data/cat/ws/kako402f-thesis/env/bin/activate
 echo "Activated environment"
-cd /home/kako402f/projects/cat/kako402f-thesis-cat/xmlc-knowledge
+cd /home/kako402f/projects/cat/kako402f-thesis/xmlc-knowledge
 
-python fs_predict.py --config configs/config_fs_ft_label_1k_1hop.yaml --result_dir results --index search_indices/label_index.pkl --mapping search_indices/label_mapping.pkl --example-type label --seed 42
+python fs_predict.py --config configs/config_fs_ft_label_3k_1hop.yaml --result_dir results --index search_indices/label_index.pkl --mapping search_indices/label_mapping.pkl --example-type label --seed 42
 
